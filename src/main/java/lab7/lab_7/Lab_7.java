fab70b07db24bd857b9a55fa26fb49aaf15b369d
Youssef-Elkady <elkadyyoussef23@gmail.com>
2022-11-16 02:21:40 +0200

    first commit

---
+++ src/main/java/lab7/lab_7/Lab_7.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package lab7.lab_7;
+import javax.swing.JFrame;
+import java.awt.*;
+/**
+ *
+ * @author taver
+ */
+public class Lab_7 {
+
+    public static void main(String[] args) {
+//        Point Position = new Point(10,50);
+//        JFrame frame = new JFrame();
+//        LineSegment L1 = new LineSegment(Position,10,Color.BLUE);
+//        frame.setVisible(true);
+          
+        
+    }
+}
